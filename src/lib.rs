e1739cf0fd8f89f3cd978d3a9078ab78d3e26f55
Héctor Ramón Jiménez <hector@hecrj.dev>
2025-04-27 10:28:40 +0200

    Add `anywho!` macro :tada:

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,29 @@
+use std::borrow::Cow;
 use std::sync::Arc;
 
 #[derive(Debug, Clone)]
 pub struct Error(Arc<dyn std::error::Error + Send + Sync + 'static>);
+
+impl Error {
+    pub fn msg(error: impl Into<Cow<'static, str>>) -> Self {
+        #[derive(Debug)]
+        struct ErrorMsg(Cow<'static, str>);
+
+        impl std::error::Error for ErrorMsg {
+            fn description(&self) -> &str {
+                &self.0
+            }
+        }
+
+        impl std::fmt::Display for ErrorMsg {
+            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                self.0.fmt(f)
+            }
+        }
+
+        Self(Arc::new(ErrorMsg(error.into())))
+    }
+}
 
 impl std::fmt::Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
@@ -30,4 +52,19 @@ where
     fn from(error: T) -> Self {
         Self(Arc::new(error))
     }
+}
+
+/// ```
+/// use anywho::{anywho, Error};
+///
+/// let a = 1;
+/// let b = 2;
+///
+/// assert_eq!(anywho!("{a} + {} = 3", b).to_string(), Error::msg("1 + 2 = 3").to_string());
+/// ```
+#[macro_export]
+macro_rules! anywho {
+    ($($arg:tt)*) => {
+        $crate::Error::msg(std::fmt::format(std::format_args!($($arg)*)))
+    };
 }
